567bcace1d0e9bd932dca0aa0cf52b1267174342
Macky <104384493+ImMacky@users.noreply.github.com>
2023-03-11 10:53:59 -0700

    client.lua v1.1

--- client/client.lua
+++ client/client.lua
@@ -1,117 +1,100 @@
 -- QBCore Exports / Variables --
-
 local QBCore = exports['qb-core']:GetCoreObject()
-
 local RecentRobbery = 0, 0, 0
-local JobInstance = false
-local LifeInvaderInstance = false
 
--- Initial Heist Setup --
-RegisterNetEvent('mk-atmRobbery:MechanicStartJob', function()
-    QBCore.Functions.TriggerCallback('mk-atmRobbery:server:PoliceInteger', function(ReturnStatus)
-        if ReturnStatus then
-            if RecentRobbery == 0 or GetGameTimer() > RecentRobbery then
-                TriggerServerEvent('mk-atmRobbery:server:RemoveMoney')
-                QBCore.Functions.Notify('Locate the Manager\'s Office, connect your laptop and fill this card out', 'success' , 5000)
-                RecentRobbery = GetGameTimer() + Config.HeistCooldown
-                JobInstance = true
-            else
-                QBCore.Functions.Notify('I have no jobs available', 'error' , 5000)
+-- ATM RFID Disuptor (Scrambler Hack) --
+RegisterNetEvent('mk-atmRobbery:UseDisruptor', function()
+    if RecentRobbery == 0 or GetGameTimer() > RecentRobbery then
+        QBCore.Functions.TriggerCallback('mk-atmRobbery:server:PoliceInteger', function(ReturnStatus)
+            if ReturnStatus then
+                QBCore.Functions.TriggerCallback('mk-atmRobbery:server:RemoveDisruptor', function(ItemChecks)
+                    if ItemChecks then
+                        QBCore.Functions.Progressbar('atm_connecting_disruptor', 'Plugging in disruptor', Config.ProgressBarInteger, false, true, {
+                            disableMovement = true,
+                            disableCarMovement = true,
+                            disableMouse = false,
+                            disableCombat = true,
+                        }, {
+                        animDict = 'anim@gangops@facility@servers@',
+                            anim = 'hotwire',
+                            flags = 49,
+                        }, {}, {}, function()
+                            ClearPedTasks(PlayerPedId())
+                            Dispatch()
+                            RecentRobbery = GetGameTimer() + Config.HeistCooldown
+                            exports['ps-ui']:Scrambler(function(success)
+                                if success then
+                                    QBCore.Functions.Progressbar('atm_grabing_cash', 'Grabing dispensed cash', Config.ProgressBarInteger, false, true, {
+                                        disableMovement = true,
+                                        disableCarMovement = true,
+                                        disableMouse = false,
+                                        disableCombat = true,
+                                    }, {
+                                    animDict = 'oddjobs@shop_robbery@rob_till',
+                                        anim = 'loop',
+                                        flags = 17,
+                                    }, {}, {}, function()
+                                        ClearPedTasks(PlayerPedId())
+                                        TriggerServerEvent('mk-atmRobbery:server:AddMoney')
+                                    end)
+                                else
+                                    QBCore.Functions.Notify('Pin incorrect', 'error' , 5000)
+                                end
+                            end, 'alphanumeric', 30, 0) -- (alphabet, numeric, alphanumeric, greek, braille, runes), Time (Seconds), Mirrored (0: Normal, 1: Normal + Mirrored 2: Mirrored only )
+                        end)
+                    end
+                end, Config.ATMRequiredItem)
             end
-        end
-    end)
-end)
-
--- Life Invader Laptop (Var Hack) --
-RegisterNetEvent('mk-atmRobbery:AdminControlPanel', function()
-    if (JobInstance == true) then
-        QBCore.Functions.TriggerCallback('mk-atmRobbery:server:RemoveLaptop', function(CheckItem)
-            if CheckItem then
-                QBCore.Functions.Progressbar('admin_credentials', 'Typing in admin credentials', 15000, false, true, {
-                    disableMovement = true,
-                    disableCarMovement = true,
-                    disableMouse = false,
-                    disableCombat = true,
-                }, {
-                animDict = 'anim@gangops@facility@servers@',
-                    anim = 'hotwire',
-                    flags = 49,
-                }, {}, {}, function()
-                    ClearPedTasks(PlayerPedId())
-                    JobInstance = false
-                    exports['ps-ui']:VarHack(function(success)
-                        if success then
-                            TriggerEvent('mk-atmRobbery:LifeInvaderPing')
-                        else
-                            QBCore.Functions.Notify('Database breach failed', 'error' , 5000)
-                        end
-                    end, 5, 3) -- (Number of Blocks), Time (Seconds)
-                end)
-            else
-                QBCore.Functions.Notify('Laptop required', 'error' , 5000)
-            end
-        end, Config.LifeInvaderVarHack)
+        end)
     else
-        QBCore.Functions.Notify('Speak with the mechanic to begin this robbery', 'error' , 5000)
+        QBCore.Functions.Notify('Disruptor use detected, come back later', 'error' , 5000)
     end
 end)
 
--- Random ATM Location (1-8)
-RegisterNetEvent('mk-atmRobbery:LifeInvaderPing', function()
-    QBCore.Functions.Notify('Location pinged, on arrival insert your Black Card', 'success' , 5000)
-    local IndexLocation = math.random(1, #Config.ATMLocations)
-    local WaypointMarker = Config.ATMLocations[IndexLocation]
-    Citizen.Wait(1000)
-
-    SetNewWaypoint(WaypointMarker)
-    LifeInvaderInstance = true
+-- Mechanic's Shop Menu (qb-menu) --
+RegisterNetEvent('mk-atmRobbery:MechanicFrontPage', function()
+    exports['qb-menu']:openMenu({
+        {
+            header = 'Suspicious Mechanic',
+            icon = 'fas fa-toolbox',
+            isMenuHeader = true,
+        },
+        {
+            header = 'Mechanic\'s Store',
+            txt = 'Purchase useful tools',
+            icon = 'fas fa-cash-register',
+            params = {
+                event = 'mk-atmRobbery:client:MechanicShopPage',
+                args = {
+                    number = 2,
+                }
+            }
+        },
+    })
 end)
 
--- ATM Black Card (Scrambler Hack) --
-RegisterNetEvent('mk-atmRobbery:BlackCardScrambler', function()
-    if (LifeInvaderInstance == true) then
-        QBCore.Functions.TriggerCallback('mk-atmRobbery:server:RemoveBlackCard', function(CheckItem)
-            if CheckItem then
-                QBCore.Functions.Progressbar('atm_connecting_database', 'Inserting Black Card', 15000, false, true, {
-                    disableMovement = true,
-                    disableCarMovement = true,
-                    disableMouse = false,
-                    disableCombat = true,
-                }, {
-                animDict = 'anim@gangops@facility@servers@',
-                    anim = 'hotwire',
-                    flags = 49,
-                }, {}, {}, function()
-                    ClearPedTasks(PlayerPedId())
-                    Dispatch()
-                    LifeInvaderInstance = false
-                    exports['ps-ui']:Scrambler(function(success)
-                        if success then
-                            QBCore.Functions.Progressbar('atm_grabing_cash', 'Grabing dispensed cash', 10000, false, true, {
-                                disableMovement = true,
-                                disableCarMovement = true,
-                                disableMouse = false,
-                                disableCombat = true,
-                            }, {
-                            animDict = 'oddjobs@shop_robbery@rob_till',
-                                anim = 'loop',
-                                flags = 17,
-                            }, {}, {}, function()
-                                ClearPedTasks(PlayerPedId())
-                                TriggerServerEvent('mk-atmRobbery:server:AddMoney')
-                            end)
-                        else
-                            QBCore.Functions.Notify('Pin incorrect', 'error' , 5000)
-                        end
-                    end, 'alphanumeric', 30, 0) -- (alphabet, numeric, alphanumeric, greek, braille, runes), Time (Seconds), Mirrored (0: Normal, 1: Normal + Mirrored 2: Mirrored only )
-                end)
-            else
-                QBCore.Functions.Notify('Black card required', 'error' , 5000)
-            end
-        end, Config.ATMKeypadScrambler)
-    else
-        QBCore.Functions.Notify('Speak with the mechanic to begin this robbery', 'error' , 5000)
-    end
+-- Mechanic's Shop Menu Purchase SubMenu (qb-menu) --
+RegisterNetEvent('mk-atmRobbery:client:MechanicShopPage', function()
+    exports['qb-menu']:openMenu({
+        {
+            header = 'Return to main menu',
+            icon = 'fas fa-chevron-left',
+            params = {
+                event = 'mk-atmRobbery:MechanicFrontPage',
+                args = {}
+            }
+        },
+        {
+            header = 'RFID Disruptor',
+            txt = 'Purchase for ($'..Config.RFIDPrice..')',
+            icon = 'fas fa-wave-square',
+            params = {
+                isServer = true,
+                event = 'mk-atmRobbery:server:PurchaseDisruptor',
+                args = {}
+            }
+        },
+    })
 end)
 
 -- ATM Robbery Disptach Notification (ps-dispatch) --
