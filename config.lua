7fcc4a5109426dab639174c7159405325ce5bef4
Macky <104384493+ImMacky@users.noreply.github.com>
2023-03-11 10:55:26 -0700

    config.lua v1.1

--- config.lua
+++ config.lua
@@ -4,31 +4,31 @@ Config = {}
 Config.HeistCooldown = (60000 * 1) -- 60 seconds * minutes
 Config.RequiredPolice = 1 -- Required Police on duty
 
-Config.InitiateHeistFee = 2500 -- Money in your bank required to begin the job
+Config.RFIDPrice = 5000 -- RFID Disruptor Price
+Config.ATMRequiredItem = 'rfid_disruptor' -- Required item for the Keypad Scrambler Hack
 
-Config.LifeInvaderVarHack = 'laptop' -- Required item for Life Invader Var Hack
-Config.ATMKeypadScrambler = 'forgedblackcard' -- Required item for ATM Keypad Scrambler Hack
+Config.ProgressBarInteger = 25000 -- Amount of time for all progress bars to complete
 
 -- Ped Config --
 Config.MechanicModel = `mp_m_waremech_01` -- Model of the Mechanic
-Config.MechanicCoords = vector3(-1040.25, -238.45, 37.96) -- Vector3 coords for the Mechanic
-Config.MechanicIcon = 'fas fa-piggy-bank' -- Icon Target for the Mechanic
-Config.MechanicLabel = 'ATM Robbery ($'..Config.InitiateHeistFee..')' -- Label Target for the Mechanic
+Config.MechanicCoords = vector3(-1369.58, -332.6, 38.97) -- Vector3 coords for the Mechanic
+Config.MechanicIcon = 'fas fa-wrench' -- Icon Target for the Mechanic
+Config.MechanicLabel = 'Speak to Mechanic' -- Label Target for the Mechanic
 
 -- ATM Config / Rewards --
-Config.ATMLocations = { -- 8 Random ATM Locations
-        vector2(-273.33, -2024.22),
-        vector2(289.37, -1256.79),
-        vector2(147.49, -1036.13),
-        vector2(-30.16, -723.38),
-        vector2(-660.65, -854.36),
-        vector2(-611.87, -705.46),
-        vector2(285.37, 143.04),
-        vector2(380.67, 322.87),
+Config.ATMProps = {
+    'prop_atm_01',
+	'prop_atm_02',
+	'prop_atm_03',
+	'prop_fleeca_atm',
 }
+
+Config.ATMIcon = 'fas fa-microchip' -- Icon Target for the ATM's
+Config.ATMLabel = 'Setup RFID Disruptor' -- Label Target for the ATM's
+Config.ATMLabelItem = 'rfid_disruptor' -- Required item for the Label to show on-thrid eye
 
 Config.RewardMoney = math.random(2000, 8000) -- Amount of cash you receive from the ATM
 
-Config.LuckyItem = 'weapon_pistol' -- Lucky Item
+Config.LuckyItem = 'cryptostick' -- Lucky Item Spawncode
 Config.LuckyItemPossibility = math.random(1, 3) -- Lucky Item Possibility
 Config.LuckyItemVariation = math.random(1, 3) -- Luck Item Variation
