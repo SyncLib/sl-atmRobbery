b33bf89097d64cb15c4eb35ec370edc9defafacf
Macky <104384493+ImMacky@users.noreply.github.com>
2023-03-11 10:54:54 -0700

    server.lua v1.1

--- server/server.lua
+++ server/server.lua
@@ -1,14 +1,13 @@
 -- QBCore Exports --
-
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Remove Money from Player's Balance to Begin the Heist --
-RegisterNetEvent('mk-atmRobbery:server:RemoveMoney', function()
+-- Remove Money from Player's Balance to purhcase the RFID --
+RegisterNetEvent('mk-atmRobbery:server:PurchaseDisruptor', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveMoney('bank', Config.InitiateHeistFee, 'Forged Black Card')
-    Player.Functions.AddItem(Config.ATMKeypadScrambler, 1)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ATMKeypadScrambler], 'add', 1)
+    Player.Functions.RemoveMoney('bank', Config.RFIDPrice, 'RFID Disruptor')
+    Player.Functions.AddItem(Config.ATMRequiredItem, 1)
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ATMRequiredItem], 'add', 1)
 end)
 
 -- Add Money to Player's Balance Once Hack is Successful --
@@ -28,29 +27,15 @@ RegisterNetEvent('mk-atmRobbery:server:AddMoney', function()
     end
 end)
 
--- Remove Laptop (Life Invader) --
-QBCore.Functions.CreateCallback('mk-atmRobbery:server:RemoveLaptop', function(source, cb, item)
+-- Remove RFID Disruptor on third eye of an ATM --
+QBCore.Functions.CreateCallback('mk-atmRobbery:server:RemoveDisruptor', function(source, cb, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local LaptopScan = Player.Functions.GetItemByName(item)
-    if LaptopScan then
+    local DisruptorScan = Player.Functions.GetItemByName(item)
+    if DisruptorScan then
         cb(true)
-        Player.Functions.RemoveItem(Config.LifeInvaderVarHack, 1)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.LifeInvaderVarHack], 'remove', 1)
-    else
-        cb(false)
-    end
-end)
-
--- Remove Black Card (ATM) --
-QBCore.Functions.CreateCallback('mk-atmRobbery:server:RemoveBlackCard', function(source, cb, item)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local BlackCardScan = Player.Functions.GetItemByName(item)
-    if BlackCardScan then
-        cb(true)
-        Player.Functions.RemoveItem(Config.ATMKeypadScrambler, 1)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ATMKeypadScrambler], 'remove', 1)
+        Player.Functions.RemoveItem(Config.ATMRequiredItem, 1)
+        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ATMRequiredItem], 'remove', 1)
     else
         cb(false)
     end
@@ -64,7 +49,7 @@ QBCore.Functions.CreateCallback('mk-atmRobbery:server:PoliceInteger', function(s
 
     for i = 1, #Player do
         local xPlayer = QBCore.Functions.GetPlayer(Player[i])
-        if (xPlayer.PlayerData.job.name == "police" and xPlayer.PlayerData.job.onduty) then
+        if (xPlayer.PlayerData.job.name == 'police' and xPlayer.PlayerData.job.onduty) then
             PoliceInteger = PoliceInteger + 1
         end
     end
