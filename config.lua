5e08e27b0bf19bc1bb5d28fe5afc3a62c52b6b42
ImMacky <mackydevelopment0@gmail.com>
2023-09-06 18:28:59 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,18 +2,17 @@ Config = {}
 
 -- Heist Config / Setup --
 Config.HeistCooldown = (60000 * 1) -- 60 seconds * minutes
-Config.RequiredPolice = 0 -- Required Police on duty
+Config.RequiredPolice = 1 -- Required Police on duty
 
-Config.RFIDPrice = 5000 -- RFID Disruptor Price
 Config.ATMRequiredItem = 'rfid_disruptor' -- Required item for the Keypad Scrambler Hack
 
 Config.ProgressBarInteger = 25000 -- Amount of time for all progress bars to complete
 
 -- Ped Config --
-Config.MechanicModel = `mp_m_waremech_01` -- Model of the Mechanic
-Config.MechanicCoords = vector3(-1369.58, -332.6, 38.97) -- Vector3 coords for the Mechanic
-Config.MechanicIcon = 'fas fa-wrench' -- Icon Target for the Mechanic
-Config.MechanicLabel = 'Speak to Mechanic' -- Label Target for the Mechanic
+Config.ElectronicsDealerModel = `a_m_y_bevhills_01` -- Model of the Electronics Dealer
+Config.ElectronicsDealerCoords = vector3(-1369.58, -332.6, 38.97) -- Vector3 coords for the Electronics Dealer
+Config.ElectronicsDealerIcon = 'fas fa-server' -- Icon Target for the Electronics Dealer
+Config.ElectronicsDealerLabel = 'Speak to Electronics Dealer' -- Label Target for the Electronics Dealer
 
 -- ATM Config / Rewards --
 Config.ATMProps = {
@@ -23,11 +22,27 @@ Config.ATMProps = {
 	'prop_fleeca_atm',
 }
 
+Config.ElectronicsDealerShop = {
+    label = "Electronics Dealer",
+    slots = 1,
+    items = {
+        [1] = {
+            name = "rfid_disruptor",
+            price = 2500,
+            amount = 5,
+            type = "item",
+            slot = 1,
+        },
+    }
+}
+
 Config.ATMIcon = 'fas fa-microchip' -- Icon Target for the ATM's
 Config.ATMLabel = 'Setup RFID Disruptor' -- Label Target for the ATM's
 Config.ATMLabelItem = 'rfid_disruptor' -- Required item for the Label to show on-thrid eye
 
-Config.RewardMoney = math.random(2000, 8000) -- Amount of cash you receive from the ATM
+Config.RewardMoney = math.random(2000, 6000) -- Amount of cash you receive from the ATM
+
+Config.ScramblerHackType = 'alphanumeric' -- (alphabet, numeric, alphanumeric, greek, braille, runes)
 
 Config.LuckyItem = 'cryptostick' -- Lucky Item Spawncode
 Config.LuckyItemPossibility = math.random(1, 3) -- Lucky Item Possibility
