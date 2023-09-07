b39b1e399e4e9fa5c12a5936400aaad1844634c6
ImMacky <mackydevelopment0@gmail.com>
2023-09-06 18:28:15 -0700

    Removed

--- server/sv_server.lua
+++ server/sv_server.lua
@@ -1,16 +1,7 @@
 -- QBCore Exports --
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Remove Money from Player's Balance to purhcase the RFID --
-RegisterNetEvent('mk-atmRobbery:server:PurchaseDisruptor', function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.RemoveMoney('bank', Config.RFIDPrice, 'RFID Disruptor')
-    Player.Functions.AddItem(Config.ATMRequiredItem, 1)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ATMRequiredItem], 'add', 1)
-end)
-
--- Add Money to Player's Balance Once Hack is Successful --
+-- Add Money to Player's Balance Once Hack is Successful / Un-Successful --
 RegisterNetEvent('mk-atmRobbery:server:AddMoney', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
@@ -18,16 +9,16 @@ RegisterNetEvent('mk-atmRobbery:server:AddMoney', function()
     local Possibility = Config.LuckyItemPossibility
     local Variation = Config.LuckyItemVariation
 
-    if Possibility == Variation then
+    --if Possibility == Variation then
         Player.Functions.AddItem(Config.LuckyItem, 1)
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.LuckyItem], 'add', 1)
         Player.Functions.AddMoney('cash', Config.RewardMoney, 'ATM withdrawl')
-    else
-        Player.Functions.AddMoney('cash', Config.RewardMoney, 'ATM withdrawl')
-    end
+    --else
+        --Player.Functions.AddMoney('cash', Config.RewardMoney, 'ATM withdrawl')
+    --end
 end)
 
--- Remove RFID Disruptor on third eye of an ATM --
+-- Remove RFID Disruptor on-third eye of an ATM --
 QBCore.Functions.CreateCallback('mk-atmRobbery:server:RemoveDisruptor', function(source, cb, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
@@ -44,7 +35,7 @@ end)
 -- Police Amount On-Duty --
 QBCore.Functions.CreateCallback('mk-atmRobbery:server:PoliceInteger', function(source, cb)
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
+    local Player = QBCore.Functions.GetPlayers()
     local PoliceInteger = 0
 
     for i = 1, #Player do
@@ -60,12 +51,4 @@ QBCore.Functions.CreateCallback('mk-atmRobbery:server:PoliceInteger', function(s
         cb(false)
         TriggerClientEvent('QBCore:Notify', src, ('Not enough police'), 'error')
     end
-end)
-
--- On Resource Start (Console Notification) --
-AddEventHandler('onResourceStart', function(resource)
-    if (GetCurrentResourceName() ~= resource) then
-        return
-    end
-    print(resource..' started successfully')
 end)
