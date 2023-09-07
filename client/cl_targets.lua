a32b0dbae03aa6cd07ef2bee439493493ae23d44
ImMacky <mackydevelopment0@gmail.com>
2023-09-06 18:28:52 -0700

    Update cl_targets.lua

--- client/cl_targets.lua
+++ client/cl_targets.lua
@@ -1,39 +1,73 @@
--- Mechanic Model / Target --
+-- Electronics Dealer Model / Target --
 CreateThread(function()
-  local model = Config.MechanicModel
-  RequestModel(model)
-  while not HasModelLoaded(model) do Wait(1000) end
+    local model = Config.ElectronicsDealerModel
+    RequestModel(model)
+    while not HasModelLoaded(model) do Wait(1000) end
 
-  local Mechanic = CreatePed(0, model, Config.MechanicCoords -1.0, 24.41, 0.0, false, false)
-  local ScenarioTable = { 'WORLD_HUMAN_TOURIST_MAP', 'WORLD_HUMAN_STAND_IMPATIENT', 'WORLD_HUMAN_DRUG_DEALER', 'WORLD_HUMAN_JANITOR' }
-  local ScenarioRequest = math.random(1, #ScenarioTable)
-  FreezeEntityPosition(Mechanic, true)
-  SetEntityInvincible(Mechanic, true)
-  SetEntityNoCollisionEntity(Mechanic, true)
-  TaskStartScenarioInPlace(Mechanic, ScenarioTable[ScenarioRequest], -1, true)
-  exports['qb-target']:AddTargetEntity(Mechanic, {
-    options = {
-      {
-        type = 'client',
-        event = 'mk-atmRobbery:MechanicFrontPage',
-        icon = Config.MechanicIcon,
-        label = Config.MechanicLabel
-      }
-    },
-    distance = 2.5,
-  })
+    local ElectronicsDealer = CreatePed(0, model, Config.ElectronicsDealerCoords -1.0, 24.41, 0.0, false, false)
+
+    -- Define a list of possible scenarios --
+    local ScenarioTable = { 
+        'WORLD_HUMAN_TOURIST_MAP',
+        'WORLD_HUMAN_STAND_IMPATIENT',
+        'WORLD_HUMAN_DRUG_DEALER',
+        'WORLD_HUMAN_JANITOR'
+    }
+    local ScenarioRequest = math.random(1, #ScenarioTable)
+
+    FreezeEntityPosition(ElectronicsDealer, true)
+    SetEntityInvincible(ElectronicsDealer, true)
+    SetEntityNoCollisionEntity(ElectronicsDealer, true)
+    TaskStartScenarioInPlace(ElectronicsDealer, ScenarioTable[ScenarioRequest], -1, true)
+
+    -- Add qb-target to ElectronicsDealer Ped --
+    exports['qb-target']:AddTargetEntity(ElectronicsDealer, {
+        options = {
+        {
+            type = 'client',
+            event = 'mk-atmRobbery:client:ElectronicsMenu',
+            icon = Config.ElectronicsDealerIcon,
+            label = Config.ElectronicsDealerLabel
+        }
+        },
+        distance = 2.5,
+    })
+end)
+
+-- Electronics Dealer Menu (qb-menu) --
+RegisterNetEvent('mk-atmRobbery:client:ElectronicsMenu', function()
+    exports['qb-menu']:openMenu({
+        {
+            header = 'Electronics Dealer',
+            icon = 'fas fa-server',
+            isMenuHeader = true,
+        },
+        {
+            header = 'Electronics Store',
+            txt = 'Purchase useful electronics',
+            icon = 'fas fa-cash-register',
+            params = {
+                event = 'mk-atmRobbery:client:openShop',
+                args = {}
+            }
+        },
+    })
+end)
+
+RegisterNetEvent('mk-atmRobbery:client:openShop', function()
+    TriggerServerEvent('inventory:server:OpenInventory', 'shop', 'Electronics Dealer', Config.ElectronicsDealerShop)
 end)
 
 -- RFID Disruptor (SCRAMBLER HACK) --
 exports['qb-target']:AddTargetModel(Config.ATMProps,  {
-  options = {
-    {
-      type = 'client',
-      event = 'mk-atmRobbery:UseDisruptor',
-      icon = Config.ATMIcon,
-      label = Config.ATMLabel,
-      item = Config.ATMLabelItem,
-    },
-  },
-  distance = 2.5,
+	options = {
+		{
+		type = 'client',
+		event = 'mk-atmRobbery:UseDisruptor',
+		icon = Config.ATMIcon,
+		label = Config.ATMLabel,
+		item = Config.ATMLabelItem,
+		},
+	},
+	distance = 2.5,
 })
